e666ad98142107d4a633136d78a0c50e58615c1c
Endreus667 <endreus.kenny.figueiro@gmail.com>
2024-10-08 23:04:33 -0300

    criaçao do formulario

---
+++ adicionar.php
@@ -0,0 +1,14 @@
+<h1>ADICIONAR USUARIO</h1>
+<form method="POST" action="adicionar_action.php">
+    <label for="">
+        nome: <br/>
+        <input type="text" name="name" />
+    </label><br/><br/>
+
+    <label for="">
+        E-mail: <br/>
+        <input type="email" name="email">
+    </label><br/><br/>
+
+    <input type="submit" value="adicionar">
+</form>
